b663aff14dd32d617ff22704253e0d97dc1ef1b2
Brogan Pratt <101632496+MrPrattASH@users.noreply.github.com>
2022-09-27 13:22:06 +0200

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+hummingbird.start_hummingbird()
+distance = 0
 
+def on_forever():
+    global distance
+    #get distance from sensor 
+    #NOTE: use a large & hard surfaced object infront of the distance sensor
+    #soft "squishy" materials will absorb the sensor ping and give bad results
+    distance = hummingbird.get_sensor(SensorType.DISTANCE, ThreePort.ONE)
+    #too close, light up red LED
+    if distance > 5 and distance < 15:
+        hummingbird.set_tri_led(TwoPort.ONE, 100, 0, 0)
+    #middle distance, light up green LED
+    elif distance >= 15 and distance <30:
+        hummingbird.set_tri_led(TwoPort.ONE, 0, 100, 0)
+    #far distance, light up blue LED
+    else:
+        hummingbird.set_tri_led(TwoPort.ONE, 0, 0, 100)
+forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+hummingbird.startHummingbird()
+let distance = 0
+forever(function on_forever() {
+    
+    // get distance from sensor 
+    // NOTE: use a large & hard surfaced object infront of the distance sensor
+    // soft "squishy" materials will absorb the sensor ping and give bad results
+    distance = hummingbird.getSensor(SensorType.Distance, ThreePort.One)
+    // too close, light up red LED
+    if (distance > 5 && distance < 15) {
+        hummingbird.setTriLED(TwoPort.One, 100, 0, 0)
+    } else if (distance >= 15 && distance < 30) {
+        // middle distance, light up green LED
+        hummingbird.setTriLED(TwoPort.One, 0, 100, 0)
+    } else {
+        // far distance, light up blue LED
+        hummingbird.setTriLED(TwoPort.One, 0, 0, 100)
+    }
+    
+})
